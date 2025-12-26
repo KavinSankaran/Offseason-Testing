8403e4d65d2b00ca04b46708f809dd3a6e1aceca
Oscar Chevalier <oscar@frozenmilk.dev>
2025-12-27 01:18:03 +1100

    chore: updated to match plugin changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.1.1-0.1.3"
+	id("dev.frozenmilk.teamcode") version "11.0.0-1.0.0"
 }
 
 ftc {
-	// use this to easily add more FTC libraries
-
 	// adds support for kotlin
-	kotlin
+	kotlin()
+
+    // adds the necessary sdk dependencies
+    sdk.TeamCode()
 }

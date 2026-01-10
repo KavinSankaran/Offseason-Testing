1899ddf3199a56bf51b189c3bca59f0aea1d2b02
Oscar Chevalier <oscar@frozenmilk.dev>
2026-01-11 09:10:19 +1100

    chore: update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "11.0.0-1.0.0"
+    id("dev.frozenmilk.teamcode") version "11.0.0-1.1.0"
 }
 
 ftc {
-	// adds support for kotlin
-	kotlin()
+    // adds support for kotlin
+    kotlin()
 
     // adds the necessary sdk dependencies
     sdk.TeamCode()
